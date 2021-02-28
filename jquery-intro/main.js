b167f8381454f6df9ca716f9cbc38289d04acc7a
Chris Gitmed <cgitmed@gmail.com>
2021-02-27 20:29:11 -0800

    Added AJAX call on button's event listener.

--- jquery-intro/main.js
+++ jquery-intro/main.js
@@ -1,6 +1,16 @@
 const $button = $('button');
 const $colorText = $('p');
+const $getWeatherButton = $('#weather-button');
 
 $button.on('click', () => {
     $colorText.toggleClass('green');
+});
+
+$getWeatherButton.on('click', () => {
+    $.ajax({
+        url: 'https://pokeapi.co/api/v2/berry/1',
+        success: (res) => {
+            console.log('res: ', res)
+        }
+    });
 });
