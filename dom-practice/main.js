1077d8107247ad7eb73034cf06b3407156333c3b
Chris Gitmed <cgitmed@gmail.com>
2021-03-08 01:06:23 -0800

    Completed the functionality for the toggle switch.

--- dom-practice/main.js
+++ dom-practice/main.js
@@ -1,8 +1,17 @@
 const $clickCounterText = document.querySelector('.click-counter > p');
 const $clickCounterButton = document.querySelector('.click-counter > button');
+const $toggleSwitch = document.querySelector('.toggle-switch');
 
 let count = 0;
 $clickCounterButton.addEventListener('click', () => {
     count++;
     $clickCounterText.textContent = `You have clicked ${count} times.`;
-})
+});
+
+$toggleSwitch.addEventListener('click', (event) => {
+    const element = event.target;
+    if (element.classList[0] === 'inner-circle') {
+        const switchBody = element.closest('.inner-border');
+        switchBody.classList.toggle('justify-end');
+    }
+});
